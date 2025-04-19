ff5a18a3901dfa05a2829d31458d3637833496d1
Ssoon-m <tkksm2@gmail.com>
2025-04-19 17:39:24 +0900

    initialize react

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,5 @@
 FROM ghcr.io/open-feature/flagd:latest
 
-# 설정 파일은 이제 S3/CloudFront에서 받아오므로 제거
-# COPY demo.flagd.json /etc/flagd/demo.flagd.json
-
 ENV FLAGD_URI=s3://flagd-config-2n7g8vmo/demo.flagd.json
 ENV FLAGD_PORT=8013
 ENV FLAGD_POLLING_INTERVAL=10s
---
+++ flagd-gui/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='flex-1 flex-col w-full bg-black h-screen'>
+        sdfdsf
+      </div>
+    </>
+  )
+}
+
+export default App
